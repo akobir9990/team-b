2a856cb85be6c7051000ba80aaf2058b7d46e68e
akobir9990 <akobir.qoshshayev@gmail.com>
2023-11-03 20:12:52 +0500

    some changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,11 @@
+import "./footer.css";
+
 function Footer() {
-  return <div className="container">footer</div>;
+  return (
+    <div className="footer-container-bg">
+      <div className="container">footer</div>
+    </div>
+  );
 }
 
 export default Footer;
