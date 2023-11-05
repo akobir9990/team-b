cfb65a94be3609eb29801cbfccf2c3efdc307d69
amirsalimov <salimovamirxon27@gmail.com>
2023-11-05 10:13:29 -0800

    sa

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,14 +1,15 @@
 import "./item.css"
 import promotionItemImg from "../../assets/images/promotionItemImg.png"
-
+import promotionrating from "../../../src/assets/images/promotionrating.png"
 function Item() {
   return (
     <div>
- <div className="card">
-<img className="" src={promotionItemImg} alt="" /><p className='card-price'>599,99$</p>
-<p className='card-name'>Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»</p>
-<img className='card-rating' src="src/copmanent/Header/img/rating (1).png" alt="" />
-<button className='card-but'>В корзину</button>
+ <div className="promotion">
+<img className="promotion-img" src={promotionItemImg} alt="" /><p className='card-price'>44,50₽ 50,50₽ </p>
+
+<p className='promotion-name'>Г/Ц Блинчики с мясом вес, Россия</p>
+<img className='promotion-rating' src={promotionrating} alt="" />
+<button className='promotion-but'>В корзину</button>
 </div>
 
     </div>
