43cae887b14da4563e91e6939c0f351527ff54a4
abdulazizthemodernworker@gmail.com <abdulazizthemodernworker@gmail.com>
2023-11-07 16:08:32 +0500

    basket-item

---
+++ src/components/basket-item/basket-item.jsx
@@ -0,0 +1,21 @@
+import "./basket-item.css"
+import itemImg from '../../assets/images/basket-item-img.png'
+
+
+const Basketitem = () => {
+  return (
+    <div className="basket-item__div">
+    <img src={ itemImg} alt="" />
+    <h2 className="basket-item__text">Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»</h2>
+    <h3 className="basket-item__text2">44,50 ₽</h3>
+      <div className="basket-item__div2">
+        <button className="basket-item__button">-</button>
+        <div className="raqam">2</div>
+        <button className="basket-item__button2">+</button>
+        <h2 className="basket-item__text3">89,00 ₽</h2>
+      </div>
+  </div>
+  )
+}
+
+export default Basketitem
