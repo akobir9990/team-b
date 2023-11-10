aba7dc562e2cc29db0ce80116247415dcddfbe1c
AnorboyevXusan <xusananor9@gmail.com>
2023-11-10 10:18:17 +0500

    ozgarishlar 3

---
+++ src/pages/home/MepsPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import MepsPages from './MepsPages';
+
+const TashkentCoordinates = {
+  latitude: 41.3111,
+  longitude: 69.2401,
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: TashkentCoordinates.latitude,
+          longitude: TashkentCoordinates.longitude,
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02,
+        }}
+      >
+        <Marker
+          coordinate={TashkentCoordinates}
+          title="Toshkent"
+          description="Oʻzbekiston, 100097"
+        />
+      </MapView>
+      <MepsPages />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default App;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 import Login from "../pages/home/LoginPage";
+import Meps from "../pages/home/MepsPage"
 
 function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} /> 
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<HomePage  />} />
+        <Route path="/home" element={<MepsPage />} />
       </Routes>
     </>
   );
